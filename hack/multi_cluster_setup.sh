588dd572390dbf5b3eb397847a9dc7fe825d8d54
Tjaarda1 <abuning@pa.uc3m.es>
2026-03-09 22:57:54 -0700

    fix license comment

--- hack/multi_cluster_setup.sh
+++ hack/multi_cluster_setup.sh
@@ -1,20 +1,20 @@
-/*
- * Derived from the ACM project
- * https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/acm/-/blob/ACM-FC/LICENSE?ref_type=heads
- *
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+#
+ # Derived from the ACM project
+ # https://gitlab.eclipse.org/eclipse-research-labs/codeco-project/acm/-/blob/ACM-FC/LICENSE?ref_type=heads
+ #
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
  
  set -euo pipefail
 
